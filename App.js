6683e94c4d426f00b1a885f5bb7c1626f422fe99
AmishaFP <amisha.agrawal@firstprinciples.io>
2023-09-11 22:11:42 +0530

    Removed the extra code and updated the code

--- App.js
+++ App.js
@@ -11,7 +11,8 @@ const MONGO_PASSWORD = process.env.MONGO_PASSWORD
 const MONGO_DB_NAME= process.env.MONGO_DB_NAME
 //const {MONGOURI} = require('./config/keys')
 const dbUrl=`mongodb+srv://${MONGO_USERNAME}:${MONGO_PASSWORD}@cluster0.dujaf.mongodb.net/${MONGO_DB_NAME}?retryWrites=true&w=majority`
-console.log(MONGO_USERNAME)
+
+
 app.use(cors())
 mongoose.connect(dbUrl,{
     useNewUrlParser:true,
@@ -29,16 +30,6 @@ require('./models/LoanDetails')
 app.use(express.json())
 app.use(require('./routes/auth'))
 app.use(require('./routes/loan'))
-//app.use(require('./routes/user'))
-
-
-// if(process.env.NODE_ENV=="production"){
-//     app.use(express.static('client/build'))
-//     const path = require('path')
-//     app.get("*",(req,res)=>{
-//         res.sendFile(path.resolve(__dirname,'client','build','index.html'))
-//     })
-// }
 
 app.listen(PORT,()=>{
     console.log("server is running on",PORT)
---
+++ README.md
@@ -0,0 +1,22 @@
+## Description
+The code base is created using Node.js and Express.js
+
+## Prerequistes
+
+Clone the git repositry to any specific folder. \
+
+Node installation is mandatory to run the server.
+
+The Node version used to create this application is v16.15.1
+
+## Installation
+
+Please run the below mentioned command to install all the dependecies used in the project.
+
+ `npm install`
+
+## Running the app
+
+Please run the below mentioned command to run the backend server
+
+ `npm run start`
